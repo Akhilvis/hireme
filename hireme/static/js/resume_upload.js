c5d84d2eba9b7868c4706544c2229cbfc9f1131d
akhil <akhilviswam000@gmail.com>
2020-05-20 22:43:11 +0530

    static files

---
+++ hireme/static/js/resume_upload.js
@@ -0,0 +1,42 @@
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: 'vue-simple-upload'
+  },
+  components: {
+    'fileupload': FileUpload.FileUpload
+  }
+})
+
+
+new Vue({
+    el: '#cand_form',
+
+    data: {
+        name: '',
+        email: '',
+        mobile: '',
+        role: '',
+        experience: ''
+    },
+
+    methods: {
+        search: function(e) {
+            e.preventDefault();
+
+            $.post("/employee/add_candidate_data", {
+                "name": this.name,
+                "email": this.email,
+                "mobile": this.mobile,
+                "role": this.role,
+                "experience": this.experience,
+                "summary": this.summary
+
+                }, function(result){
+                $("span").html(result);
+              });
+
+        }
+    }
+});
