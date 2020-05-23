338eb90b001b8abb477bd2fb121ca75180d78624
akhil <akhilviswam000@gmail.com>
2020-05-23 22:56:04 +0530

    debugging

--- hireme/home/views.py
+++ hireme/home/views.py
@@ -43,6 +43,7 @@ def LoadRecentCandidates(request):
 
 def get_resume_url(resume_id):
     resume_url = CandidateResume.objects.get(pk=resume_id).resume.url
+    resume_url = 'media/' + resume_url.split('media/')[2]
     print("Resume url.... ", resume_url)
 
     return  CandidateResume.objects.get(pk=resume_id).resume.url
