695812ecd34c17fd738684437f978fe6d51ad12d
akhil <akhilviswam000@gmail.com>
2020-05-23 21:29:31 +0530

    allowed host changed

--- hireme/hr/views.py
+++ hireme/hr/views.py
@@ -28,6 +28,7 @@ def SearchCandidate(request):
     if keyword:
         all_roles, all_role_ids = CandidateBasicInfo.get_all_roles()
         top3_candidates = process.extract(modified_keyword, all_roles)[:3]
+        print('top3_candidates....   ', top3_candidates)
         candidates_ids = [all_roles.index(cand[0])+1 for cand in top3_candidates]
         all_candidates = CandidateBasicInfo.objects.filter(id__in=candidates_ids).order_by('-id')[:3].values()
 
